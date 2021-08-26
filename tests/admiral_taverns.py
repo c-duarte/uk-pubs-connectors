3ec9b5449b172271d64e66f4406926056c6d6496
Carlos Alberto Duarte Pinto <carlos.adpinto@gmail.com>
2021-08-26 00:33:07 -0300

    Finish getter and cleaner

--- tests/admiral_taverns.py
+++ tests/admiral_taverns.py
@@ -1,9 +1,42 @@
 import unittest
 
-from uk_pubs_connectors.admiraltaverns import AdmiralTavernsConnector
+import pandas
+
+from uk_pubs_connectors.admiral_taverns import AdmiralTavernsConnector
 
 
-class TestAdmiralTaverns(unittest.TestCase):
-    def test(self) -> None:
+class TestRawData(unittest.TestCase):
+    data: pandas.DataFrame
+
+    def setUp(self) -> None:
         connector = AdmiralTavernsConnector()
-        connector.get()
+        self.data = connector.get()
+
+    def test_null(self) -> None:
+        null_data = self.data.loc[self.data.isna().any(axis=1)]\
+            .to_dict('records')
+
+        self.assertListEqual(null_data, [])
+
+    def test_approximate_price(self) -> None:
+        invalid = self.data.loc[
+            ~self.data['ApproximatePrice']
+            .str.match(r'Approximate Ingoings [£]?[\d,]+'),
+            ['ApproximatePrice']
+        ]
+        invalid = invalid.reset_index().to_dict('records')
+
+        self.assertListEqual(invalid, [])
+
+
+class TestTransformed(unittest.TestCase):
+    data: pandas.DataFrame
+
+    def setUp(self) -> None:
+        connector = AdmiralTavernsConnector()
+        self.data = connector.clean(connector.get())
+        self.data.to_csv('env\\foo.csv', index=False)
+
+    def test_values(self) -> None:
+        print(self.data['AnnualRent'].max())
+        print(self.data['AnnualRent'].min())
--- uk_pubs_connectors/admiral_taverns.py
+++ uk_pubs_connectors/admiral_taverns.py
@@ -1,17 +1,49 @@
+import unittest
 import pandas
 import requests
 
 from lxml import html
 
-from utils import mount_html_elements
+from uk_pubs_connectors.utils import mount_html_elements
 
 
 class AdmiralTavernsConnector:
     '''Connector with Admiral Taverns data source for pubs in the UK.'''
+    NAME = 'Admiral Taverns'
     URL = 'https://www.admiraltaverns.co.uk/find-a-pub/?pcSearch&z=2&ppp=-1'
     STRUCTURE = {
-        'Pubs': './/div[@class = "newsArticle table"]'
+        'Pubs': [
+            './/div[@class = "newsArticle table"]',
+            {
+                'Name': './div/a/text()',
+                'URL': './div/a/@href',
+                'StreetAddress': './/p[@class = "location"]/text()',
+                'ApproximatePrice': './/p[@class = "price"]/text()',
+                'Description': './/div[@class = "excerpt"]/text()'
+            }
+        ]
     }
+
+    def clean(self, raw_data: pandas.DataFrame) -> pandas.DataFrame:
+        '''Clean the raw data from the website.
+
+        :param raw_data: Raw data from the website
+        :type raw_data: pandas.DataFrame
+        :return: Clean data
+        :rtype: pandas.DataFrame
+        '''
+        data = raw_data.copy()
+
+        data['AnnualRent'] = data['ApproximatePrice']\
+            .str.replace(r'Approximate Ingoings [£]?', '')\
+            .str.replace(r'[.,]', '')\
+            .astype('float64')
+
+        del data['ApproximatePrice']
+
+        data['source'] = self.NAME
+
+        return data
 
     def get(self) -> pandas.DataFrame:
         response = requests.get(self.URL)
@@ -19,7 +51,8 @@ class AdmiralTavernsConnector:
 
         page_elements = mount_html_elements(html_obj, self.STRUCTURE)
 
-        print(len(page_elements['Pubs']))
-        print(page_elements['Pubs'][0])
+        pubs_list = page_elements['Pubs']
 
-        return pandas.DataFrame()
+        data = pandas.DataFrame(pubs_list).applymap(', '.join)
+
+        return data
